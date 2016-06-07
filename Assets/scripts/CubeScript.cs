a099de26ef17b1cb62c5a326c4026b4ac1886154
André Borba Netto Assis <Andr Borba Netto Assis>
2016-06-06 23:54:44 -0300

    Geração do MasterCubo Genérico. Identificação dos cubos de quina, externos e internos.

---
+++ Assets/scripts/CubeScript.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+    public float spaceBetweenCubes;
+    public int cubeSize;
+    public int[][][] masterCube;
+
+	// Use this for initialization
+	void Start () {
+        masterCube = createCube(masterCube, cubeSize);
+        drawCube(masterCube);
+       
+    }
+
+    public int[][][] createCube(int [][][] masterCube, int cubeSize)
+    {
+        masterCube = new int[cubeSize][][];
+
+        for (int i = 0; i < masterCube.Length; i++)
+        {
+            masterCube[i] = new int[cubeSize][];
+            for (int j = 0; j < masterCube[i].Length; j++)
+            {
+                masterCube[i][j] = new int[cubeSize];
+
+                for (int k = 0; k < masterCube[i][j].Length; k++)
+                { 
+                    masterCube[i][j][k] = new int();
+                }
+            }
+        }
+
+        return masterCube;
+    }
+
+    public void drawCube(int [][][] masterCube)
+    {
+        for (int i = 0; i < masterCube.Length; i++)
+        {
+            for (int j = 0; j < masterCube[i].Length; j++)
+            {
+
+                for (int k = 0; k < masterCube[i][j].Length; k++)
+                {
+                    
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.parent = gameObject.transform;
+                    cube.transform.position = new Vector3(i, j, k);
+                    cube.GetComponent<Transform>().localScale = new Vector3(cube.GetComponent<Transform>().localScale.x - spaceBetweenCubes, cube.GetComponent<Transform>().localScale.y - spaceBetweenCubes, cube.GetComponent<Transform>().localScale.z - spaceBetweenCubes);
+                    
+                    
+
+                    cube.GetComponent<Renderer>().material = new Material(Shader.Find("Standard"));
+                    cube.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f, 0.6f);
+                    cube.GetComponent<Renderer>().material.SetFloat("_Mode", 3);
+
+                    
+                    cube.GetComponent<Renderer>().material.SetFloat("_Metallic", 0f);
+                    cube.GetComponent<Renderer>().material.SetFloat("_Glossiness", 0.115f);
+                    cube.name = i.ToString() + j.ToString() + k.ToString();
+                    cube.AddComponent<singleCubeScript>();
+                    /*
+                    cube.GetComponent<Renderer>().material.EnableKeyword("_METALLICGLOSSMAP");
+                    cube.GetComponent<Renderer>().material.SetFloat("_METALLICGLOSSMAP", 0.5f);
+                    */
+
+
+                }
+            }
+        }
+       
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
---
+++ Assets/scripts/singleCubeScript.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class singleCubeScript : MonoBehaviour {
+
+    public int masterCubeSize;
+
+    public bool externCube;
+    public bool internCube;
+    public bool cornerCube;
+
+
+	// Use this for initialization
+	void Start () {
+
+        masterCubeSize = gameObject.GetComponentInParent<CubeScript>().cubeSize;
+        identifyCubeType(gameObject.name, masterCubeSize-1);
+        paintCube();
+
+       
+	}
+
+    
+
+    public void identifyCubeType(string position, int masterCubeSize)
+    {
+        
+
+        //Identify CornerCubes.
+        bool cornerCb = true;
+        bool externCb = false;
+        bool internCb = false;
+
+        for (int i = 0; i < 3; i++)
+        {
+            //Se ele tiver alguma posição que seja diferente de 0 e diferente do (máximo tamanho do cubo-1)
+            if (position[i].ToString() != "0" && position[i].ToString() != masterCubeSize.ToString())
+            {
+                //Então ele não é um cubo de quina.
+                cornerCb = false;
+            }
+            //Senão, se ele tiver alguma posição 0 ou (máximo tamanho do cubo-1).
+            else
+            {
+                //Então ele é um cubo externo.
+                externCb = true;
+            }
+           
+        }
+
+
+
+        if (cornerCb)
+        {
+            cornerCube = true;
+            externCube = true;
+            internCube = false;
+        }
+        else if (externCb)
+        {
+            cornerCube = false;
+            externCube = true;
+            internCube = false;
+        }
+        else
+        {
+            internCube = true;
+            cornerCube = false;
+            externCube = false;
+        }
+    }
+
+
+    void paintCube()
+    {
+        if (cornerCube)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else if (externCube)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        else if(internCube)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
